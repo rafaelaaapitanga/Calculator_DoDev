8744695bc7097b53c87dd326771463a898e754be
Rafaela Pitanga <rafa.pitanga@hotmail.com>
2024-01-24 19:46:01 -0300

    entregavel - calculadora

--- src/script.js
+++ src/script.js
@@ -1,7 +1,38 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(n1, n2, operador){
+  let resultado;
+  let num1 = parseInt(n1);
+  let num2 = parseInt(n2);
+
+  switch(operador){
+    case '+':
+      resultado = num1 + num2;
+      break;
+    case '-':
+      resultado = num1 - num2;
+      break;
+    case '*':
+      resultado = num1 * num2;
+      break;
+    case '/':
+      resultado = num1 / num2;
+      break;
+    case 'e':
+      resultado = 1;
+      for (let i = 0; i < num2; i++) {
+        resultado *= num1; // Isso é feito repetidamente até que o número de iterações seja igual ao expoente (num2)
+        // a cada iteração, o resultado é multiplicado por num1 (base)
+      }
+      break;
+  }
+  if (resultado == undefined || resultado > 1000000)
+    resultado == 'ERRO'
+
+  return resultado
+  
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
